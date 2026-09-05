66dfd0d47d260725ef49bc6990b5eef83ba66abe
SolidBug3 <milosd21000@gmail.com>
2026-09-06 00:59:46 +0200

    a propos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './css/index.css'
 import './css/header.css';
 import './css/home_center_image.css';
 import './css/speech_bubble.css';
+import './css/section.css';
 
 function App() {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -17,10 +18,10 @@ function App() {
 
 
                     <nav className={menuOpen ? 'menu-open' : ''}>
-                        <a href="#home" onClick={() => setMenuOpen(false)}>Accueuil</a>
+                        <a href="#homepage" onClick={() => setMenuOpen(false)}>Accueuil</a>
                         <a href="#about" onClick={() => setMenuOpen(false)}>A propos</a>
                         <a href="#projects" onClick={() => setMenuOpen(false)}>Example de projets</a>
-                        <a href="#contact" onClick={() => setMenuOpen(false)}>Me contacter</a>
+                        <a href="#contact" onClick={() => setMenuOpen(false)}>Travaillons ensemble</a>
                     </nav>
 
                     <button
@@ -35,14 +36,35 @@ function App() {
 
 
             <main id="home">
+            <section id ="homepage">
                 <div className="hero-image">
                     <div className="speech-bubble">
                         <span>À Dijon, on ne fait pas que de la moutarde.</span>
                         <span>On développe aussi vos applications..</span>
                     </div>
 
-                    <img src={`${import.meta.env.BASE_URL}me.png`} alt="" />
-                </div>
+                        <img src={`${import.meta.env.BASE_URL}me.png`} alt="" />
+                    </div>
+                </section>
+
+                <section id="about">
+                    <div className="about_info">
+                        Moi c’est Milo, aka <strong>SolidBug</strong> !<br /><br />
+                        Un artiste à qui on a donné un clavier..<br />
+                        Et j’ai fini par en faire mon métier (rire)<br />
+                        Et puis est née une passion et une <strong>éthique de code</strong>.<br /><br />
+                        Un chasseur de « bug », mais pas ceux qui chantent en été.<br /><br />
+                        Je matérialise vos projets à coups de clavier, de marteau et d’amour (sûrement)<br />
+                        L’image de votre entreprise, votre « brand », comme l’on dirait de nos jours,<br />
+                        je la façonne, la bichonne et lui donne forme sous une belle <strong>UI / UX</strong>.<br /><br />
+                        L’art a la réputation de ne pas s’entremêler avec l’ombre de la science,<br />
+                        eh bien c’est ici que vous les trouverez au rendez-vous,<br />
+                        comme vous et moi, si je puis avoir l’honneur de travailler sur votre projet.<br /><br />
+                        Au plaisir de vous voir pour de nouvelles aventures codesques et graphiques !<br /><br />
+                        PS : Comme vous l’aurez compris de par ma plume, les langues et l’écriture sont d’autres cordes à mon arc.<br />
+                        Je peux traduire vos applications en <strong>anglais</strong> et <strong>japonais</strong>.
+                    </div>
+                </section>
             </main>
         </>
     )
