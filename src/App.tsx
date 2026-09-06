50acf6c3396795b413bc788c36f8248d9d06dea9
SolidBug3 <milosd21000@gmail.com>
2026-09-06 23:42:52 +0200

    better home page text

--- src/App.tsx
+++ src/App.tsx
@@ -205,11 +205,9 @@ function App() {
                             Un artiste à qui on a donné un clavier..<br />
                             Et j’ai fini par en faire mon métier (rire)<br />
                             Et puis est née une passion et une <strong>éthique de code</strong>.<br /><br />
-                            Un chasseur de « bug », mais pas ceux qui chantent en été.<br /><br />
-                            Je matérialise vos projets à coups de clavier, de café et d’amour (sûrement)<br />
-                            L’image de votre entreprise, votre « brand », comme l’on dirait de nos jours,<br />
-                            je la façonne, la bichonne et lui donne forme sous une belle <strong>UI / UX</strong>.<br /><br />
-                            Au plaisir de vous voir pour de nouvelles aventures codesques et graphiques !<br /><br />
+                            Un chasseur de « bug », mais pas ceux qui chantent en été.<br />
+                            Celui qui faconne l'image des projets et leur donne vie.<br />Ou plutot UI.<br /><br />
+                            Au plaisir de vous voir pour des aventures codesques et graphiques !<br /><br />
                             <strong>PS</strong> : Comme vous l’aurez compris de par ma plume, les langues et l’écriture sont d’autres cordes à mon arc.<br /><br />
                             Je peux traduire vos applications en <strong>anglais</strong> et <strong>japonais</strong>.
 
