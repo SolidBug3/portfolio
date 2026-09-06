3e92ac9dd31154668fdfa75debb373b47417de05
SolidBug3 <milosd21000@gmail.com>
2026-09-06 19:55:06 +0200

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function App() {
                     <nav className={menuOpen ? 'menu-open' : ''}>
                         <a href="#homepage" onClick={() => setMenuOpen(false)}>Accueuil</a>
                         <a href="#skills" onClick={() => setMenuOpen(false)}>Compétences</a>
-                        <a href="#projects" onClick={() => setMenuOpen(false)}>Exemple de projets</a>
+                        <a href="#projects" onClick={() => setMenuOpen(false)}>Projets</a>
                         <a href="#contact" onClick={() => setMenuOpen(false)}>Travaillons ensemble</a>
                         <a href="#about" onClick={() => setMenuOpen(false)}>A propos</a>
                     </nav>
