ea6a6f6f7579db869e8a313cf8f4e6a2204ae76b
SolidBug3 <milosd21000@gmail.com>
2026-09-07 14:27:21 +0200

    broken google button fix

--- memodget/pages/Home.tsx
+++ memodget/pages/Home.tsx
@@ -3,13 +3,16 @@ import '../css/background.css'
 
 import GoogleButton from '../components/GoogleButton'
 import Presentation from '../components/Presentation'
-
+import type HomeProps from '../components/Session'
 
 export default function Home({ user, handleGoogleLogin, handleLogout }: HomeProps) {
     return (
         <div>
             <div className="header">
-                <GoogleButton />
+                <GoogleButton
+                    user={user}
+                    handleGoogleLogin={handleGoogleLogin}
+                    handleLogout={handleLogout} />
             </div>
 
             {user ? (<div> You are connected </div> ) : ( <Presentation /> )}
